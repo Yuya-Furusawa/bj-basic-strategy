bb0da3a5f8398f7f1da59fae3e7786b85ae8bc88
Yuya Furusawa <fullyou0798@gmail.com>
2026-01-19 00:23:28 +0900

    feat: set-up storybook

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
 import { Stack } from 'expo-router';
 import React from 'react';
+import { STORYBOOK_ENABLED } from '../constants/storybook';
 
 export default function RootLayout() {
+  if (STORYBOOK_ENABLED) {
+    const StorybookUIRoot = require('../.rnstorybook').default;
+    return <StorybookUIRoot />;
+  }
+
   return (
     <Stack
       screenOptions={{
