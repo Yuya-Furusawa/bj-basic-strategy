ac96e1bffeaeefc74772075934fe292dd3cdc925
Yuya Furusawa <fullyou0798@gmail.com>
2026-02-05 15:01:32 +0900

    fix config

--- app.config.ts
+++ app.config.ts
@@ -69,6 +69,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       {
         androidAppId: admobIds.android,
         iosAppId: admobIds.ios,
+        userTrackingUsageDescription: false,
       },
     ],
   ],
